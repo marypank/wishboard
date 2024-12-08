3d82d1749f6b7ee3e3f97e4b8f80f5df15217dc4
Maria Pankova <qitelopdo@gmail.com>
2024-12-08 18:25:57 +0300

    controllers: new empty controllers

---
+++ controllers/AuthController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace app\controllers;
+
+use yii\web\Controller;
+use Yii;
+
+class AuthController extends Controller
+{
+    // todo: перенести в сервис
+    public function actionLigin()
+    {
+        //
+    }
+
+    public function actionRegister()
+    {
+        //
+    }
+
+    public function actionLogout()
+    {
+        //
+    }
+}
